4adf90347bdf8a11decfbf727a9cd80e8f3185b8
shibam17 <shibam.dipu@gmail.com>
2020-12-12 15:00:50 +0530

    added

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ console.log("hello");
 
 const btn = document.querySelector(".button");
 
-btn.addEventListener('onclick', ()=>{
+btn.addEventListener('click', ()=>{
     location.href="./game.html"
+    console.log("kk");
 })
